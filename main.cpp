bfada7d1652f4fc947d41bbfdea626d484129d71
aszkid <polgomezriquelme@gmail.com>
2018-08-18 17:04:43 -0500

    Drawing some text

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ using std::endl;
 
 int main(int argc, char** argv)
 {
-	std::ifstream infile("../run/config.json");
+	const std::string prefix = "../run/";
+
+	std::ifstream infile(prefix + "config.json");
 	if (!infile) {
 		cerr << "Failed to open config file!" << endl;
 		return 1;
@@ -22,9 +24,22 @@ int main(int argc, char** argv)
 	int s_width =  j["screen"]["width"];
 	int s_height =  j["screen"]["height"];
 
+	sf::Font font;
+	if (!font.loadFromFile(prefix + "fonts/Roboto/Roboto-Regular.ttf")) {
+		cerr << "Failed to open font file!" << endl;
+		return 1;
+	}
+
 	sf::RenderWindow window(sf::VideoMode(s_width, s_height), "7 Days of Doom");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString("7 Days of Doom");
+	text.setCharacterSize(24);
+	text.setFillColor(sf::Color::White);
+	text.setPosition(200.f, 200.f);
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -35,6 +50,7 @@ int main(int argc, char** argv)
 
 		window.clear();
 		window.draw(shape);
+		window.draw(text);
 		window.display();
 	}
 
