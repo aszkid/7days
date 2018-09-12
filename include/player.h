a274d099ba8bf709e105e072467de1de48c21d0b
Oriol <oriolventosauab@gmail.com>
2018-09-12 22:44:49 +0200

    Algorisme per detectar colisions de les caixes

--- include/player.h
+++ include/player.h
@@ -1,16 +1,23 @@
 #pragma once
 
+#include <vector>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/Vector2.hpp>
 
 #include <jdm/path.h>
 
+struct CollisionRect {
+        sf::Vector2f size;
+        sf::Vector2f pos;
+};
+
+
 struct Player {
-	sf::Vector2f p_position;
-	sf::Texture p_texture;
-	
-	float p_width;
-	float p_height;
+	sf::Vector2f position;
+	sf::Vector2f size;
+	sf::Texture texture;
 
 	static bool from_json(Player *player,  const jdm::path &file);
+	bool can_move(Player player, std::vector<CollisionRect> collision_rect);
+	bool dim1(sf::Vector2f p, sf::Vector2f b);
 };
--- src/player.cpp
+++ src/player.cpp
@@ -19,16 +19,48 @@ bool Player::from_json(Player *p, const jdm::path &file)
 	json j;
 	infile >> j;
 
-	p -> p_width = j["width"];
-	p -> p_height = j["height"];
-	p -> p_position.x = j["x"];
-	p -> p_position.y = j["y"];
+	p -> size.x = j["width"];
+	p -> size.y = j["height"];
+	p -> position.x = j["x"];
+	p -> position.y = j["y"];
 	
 	const std::string player_texture = j["image"];
-	if(!p -> p_texture.loadFromFile(file / ("../" + player_texture))) {
+	if(!p -> texture.loadFromFile(file / ("../" + player_texture))) {
 		//No se com funciona lo dels paths encara
 		log -> error("Failed at loading player texture!");
 	}
 	
+	return true;
+}
+
+bool Player::can_move(Player p, std::vector<CollisionRect> rect) // S'ha de passar la posició on estarà, no en la que esta
+{
+	sf::Vector2f px_cords(p.position.x, p.position.x + p.size.x);
+	sf::Vector2f py_cords(p.position.y, p.position.y + p.size.y);
+	sf::Vector2f bx_cords;
+	sf::Vector2f by_cords;
+
+	for(size_t i=0 ; i < rect.size(); i++ ){
+		bx_cords.x = rect[i].pos.x;
+		bx_cords.y = rect[i].pos.x + rect[i].size.x; 
+		if(!dim1(px_cords, bx_cords)){
+	                by_cords.x = rect[i].pos.y;
+	                by_cords.y = rect[i].pos.y + rect[i].size.y;
+			if(!dim1(py_cords, by_cords)){
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+
+
+bool Player::dim1(sf::Vector2f player, sf::Vector2f box)
+{
+	if(player.x < box.y && box.x<player.y){
+		return false;
+	}
 	return true;
 }
